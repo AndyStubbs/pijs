fa6b251f371eedd993294612e5c0f8872a356cf8
Andy <astubbs50@gmail.com>
2024-11-18 20:37:27 -0700

    Migrated repo from AndysCodeVault to here

---
+++ test/dtest.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Deleting test\tests\screenshots\draw_02.png
+del c:\docs\src\codevault\pijs\test\tests\screenshots\draw_02.png
+echo Renaming c:\docs\src\codevault\pijs\test\tests\screenshots\draw_02_new.png
+move c:\docs\src\codevault\pijs\test\tests\screenshots\draw_02_new.png c:\docs\src\codevault\pijs\test\tests\screenshots\draw_02.png
---
+++ test/dtest2.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Deleting c:\docs\src\codevault\pijs\test\tests\screenshots\draw_02.png
+del c:\docs\src\codevault\pijs\test\tests\screenshots\draw_02_new.png
+echo Deleting c:\docs\src\codevault\pijs\test\tests\screenshots\draw_02_new.png
+del c:\docs\src\codevault\pijs\test\tests\screenshots\draw_02.png
