a3ad7ed1b6e06377e661ec902f894a3c918dff39
Andy <astubbs50@gmail.com>
2025-10-11 17:58:10 -0600

    Remove key map and using standard code

--- src/modules/keyboard.js
+++ src/modules/keyboard.js
@@ -24,133 +24,6 @@ export function init( pi ) {
 	// Keyboard started flag
 	let keyboardStarted = false;
 
-	// Key lookup table (character/special key to code mapping)
-	const m_keyLookup = buildKeyLookup();
-
-	function buildKeyLookup() {
-		return {
-			"Alt_1": "AltLeft",
-			"Alt_2": "AltRight",
-			"ArrowDown_0": "ArrowDown",
-			"ArrowLeft_0": "ArrowLeft",
-			"ArrowRight_0": "ArrowRight",
-			"ArrowUp_0": "ArrowUp",
-			"\\_0": "Backslash",
-			"|_0": "Backslash",
-			"~_0": "Backquote",
-			"`_0": "Backquote",
-			"Backspace_0": "Backspace",
-			"[_0": "BracketLeft",
-			"{_0": "BracketLeft",
-			"]_0": "BracketRight",
-			"}_0": "BracketRight",
-			"CapsLock_0": "CapsLock",
-			"ContextMenu_0": "ContextMenu",
-			"Control_1": "ControlLeft",
-			"Control_2": "ControlRight",
-			",_0": "Comma",
-			"<_0": "Comma",
-			"Delete_0": "Delete",
-			")_0": "Digit0",
-			"0_0": "Digit0",
-			"1_0": "Digit1",
-			"!_0": "Digit1",
-			"2_0": "Digit2",
-			"@_0": "Digit2",
-			"3_0": "Digit3",
-			"#_0": "Digit3",
-			"4_0": "Digit4",
-			"$_0": "Digit4",
-			"5_0": "Digit5",
-			"%_0": "Digit5",
-			"6_0": "Digit6",
-			"^_0": "Digit6",
-			"7_0": "Digit7",
-			"&_0": "Digit7",
-			"8_0": "Digit8",
-			"*_0": "Digit8",
-			"9_0": "Digit9",
-			"(_0": "Digit9",
-			"End_0": "End",
-			"Enter_0": "Enter",
-			"+_0": "Equal",
-			"=_0": "Equal",
-			"Escape_0": "Escape",
-			"F1_0": "F1",
-			"F2_0": "F2",
-			"F3_0": "F3",
-			"F4_0": "F4",
-			"F5_0": "F5",
-			"F6_0": "F6",
-			"F7_0": "F7",
-			"F8_0": "F8",
-			"F9_0": "F9",
-			"F10_0": "F10",
-			"F11_0": "F11",
-			"F12_0": "F12",
-			"Home_0": "Home",
-			"Insert_0": "Insert",
-			"a_0": "KeyA", "A_0": "KeyA",
-			"b_0": "KeyB", "B_0": "KeyB",
-			"c_0": "KeyC", "C_0": "KeyC",
-			"d_0": "KeyD", "D_0": "KeyD",
-			"e_0": "KeyE", "E_0": "KeyE",
-			"f_0": "KeyF", "F_0": "KeyF",
-			"g_0": "KeyG", "G_0": "KeyG",
-			"h_0": "KeyH", "H_0": "KeyH",
-			"i_0": "KeyI", "I_0": "KeyI",
-			"j_0": "KeyJ", "J_0": "KeyJ",
-			"k_0": "KeyK", "K_0": "KeyK",
-			"l_0": "KeyL", "L_0": "KeyL",
-			"m_0": "KeyM", "M_0": "KeyM",
-			"n_0": "KeyN", "N_0": "KeyN",
-			"o_0": "KeyO", "O_0": "KeyO",
-			"p_0": "KeyP", "P_0": "KeyP",
-			"q_0": "KeyQ", "Q_0": "KeyQ",
-			"r_0": "KeyR", "R_0": "KeyR",
-			"s_0": "KeyS", "S_0": "KeyS",
-			"t_0": "KeyT", "T_0": "KeyT",
-			"u_0": "KeyU", "U_0": "KeyU",
-			"v_0": "KeyV", "V_0": "KeyV",
-			"w_0": "KeyW", "W_0": "KeyW",
-			"x_0": "KeyX", "X_0": "KeyX",
-			"y_0": "KeyY", "Y_0": "KeyY",
-			"z_0": "KeyZ", "Z_0": "KeyZ",
-			"-_0": "Minus",
-			"__0": "Minus",
-			"._0": "Period",
-			">_0": "Period",
-			"'_0": "Quote",
-			"\"_0": "Quote",
-			";_0": "Semicolon",
-			":_0": "Semicolon",
-			"Shift_1": "ShiftLeft",
-			"Shift_2": "ShiftRight",
-			"/_0": "Slash",
-			"?_0": "Slash",
-			" _0": "Space",
-			"Tab_0": "Tab",
-			"Numpad0_0": "Numpad0",
-			"Numpad1_0": "Numpad1",
-			"Numpad2_0": "Numpad2",
-			"Numpad3_0": "Numpad3",
-			"Numpad4_0": "Numpad4",
-			"Numpad5_0": "Numpad5",
-			"Numpad6_0": "Numpad6",
-			"Numpad7_0": "Numpad7",
-			"Numpad8_0": "Numpad8",
-			"Numpad9_0": "Numpad9",
-			"NumpadAdd_0": "NumpadAdd",
-			"NumpadDecimal_0": "NumpadDecimal",
-			"NumpadDivide_0": "NumpadDivide",
-			"NumpadEnter_0": "NumpadEnter",
-			"NumpadMultiply_0": "NumpadMultiply",
-			"NumpadSubtract_0": "NumpadSubtract",
-			"PageDown_0": "PageDown",
-			"PageUp_0": "PageUp"
-		};
-	}
-
 	function startKeyboard() {
 		if( keyboardStarted ) {
 			return;
@@ -252,14 +125,29 @@ export function init( pi ) {
 	}
 
 	function triggerKeyEventListeners( mode, key, code ) {
-		// Trigger specific key listeners
-		const codeListeners = m_onKeyEventListeners[ code ];
-		if( codeListeners && codeListeners[ mode ] ) {
-			for( let i = codeListeners[ mode ].length - 1; i >= 0; i-- ) {
-				const listener = codeListeners[ mode ][ i ];
+		
+		// Trigger listeners registered for the key character (e.g., "a", ";")
+		const keyListeners = m_onKeyEventListeners[ key ];
+		if( keyListeners && keyListeners[ mode ] ) {
+			for( let i = keyListeners[ mode ].length - 1; i >= 0; i-- ) {
+				const listener = keyListeners[ mode ][ i ];
 				listener.fn( { "key": key, "code": code, "mode": mode } );
 				if( listener.once ) {
-					codeListeners[ mode ].splice( i, 1 );
+					keyListeners[ mode ].splice( i, 1 );
+				}
+			}
+		}
+
+		// Trigger listeners registered for the code (e.g., "KeyA", "Semicolon")
+		if( code && code !== key ) {
+			const codeListeners = m_onKeyEventListeners[ code ];
+			if( codeListeners && codeListeners[ mode ] ) {
+				for( let i = codeListeners[ mode ].length - 1; i >= 0; i-- ) {
+					const listener = codeListeners[ mode ][ i ];
+					listener.fn( { "key": key, "code": code, "mode": mode } );
+					if( listener.once ) {
+						codeListeners[ mode ].splice( i, 1 );
+					}
 				}
 			}
 		}
@@ -287,13 +175,16 @@ export function init( pi ) {
 
 		// If the key is provided then return the key status
 		if( key != null ) {
+			// Check by character first (e.g., "a", ";", ":")
 			if( m_keys[ key ] ) {
 				return m_keys[ key ];
 			}
+			// Then check by .key property
 			if( m_keyKeys[ key ] ) {
 				return m_keyKeys[ key ];
 			}
-			return m_keyCodes[ key ];
+			// Finally check by .code property (e.g., "KeyA", "Semicolon")
+			return m_keyCodes[ key ] || false;
 		}
 
 		// If no key is provided then return next key from input buffer
@@ -331,17 +222,16 @@ export function init( pi ) {
 			return;
 		}
 
-		// Resolve key to code
-		const code = m_keyLookup[ key + "_0" ] || key;
-
-		if( !m_onKeyEventListeners[ code ] ) {
-			m_onKeyEventListeners[ code ] = {};
+		// Use key directly - can be a character ("a", ";") or code ("KeyA", "Semicolon")
+		// Register under the key value provided
+		if( !m_onKeyEventListeners[ key ] ) {
+			m_onKeyEventListeners[ key ] = {};
 		}
-		if( !m_onKeyEventListeners[ code ][ mode ] ) {
-			m_onKeyEventListeners[ code ][ mode ] = [];
+		if( !m_onKeyEventListeners[ key ][ mode ] ) {
+			m_onKeyEventListeners[ key ][ mode ] = [];
 		}
 
-		m_onKeyEventListeners[ code ][ mode ].push( { "fn": fn, "once": once } );
+		m_onKeyEventListeners[ key ][ mode ].push( { "fn": fn, "once": once } );
 	}
 
 	// OFFKEY - Remove key event listener
@@ -366,17 +256,15 @@ export function init( pi ) {
 			return;
 		}
 
-		// Resolve key to code
-		const code = m_keyLookup[ key + "_0" ] || key;
-
-		if( m_onKeyEventListeners[ code ] && m_onKeyEventListeners[ code ][ mode ] ) {
+		// Remove listener registered under this key
+		if( m_onKeyEventListeners[ key ] && m_onKeyEventListeners[ key ][ mode ] ) {
 			if( fn ) {
-				m_onKeyEventListeners[ code ][ mode ] = 
-					m_onKeyEventListeners[ code ][ mode ].filter(
+				m_onKeyEventListeners[ key ][ mode ] = 
+					m_onKeyEventListeners[ key ][ mode ].filter(
 						listener => listener.fn !== fn
 					);
 			} else {
-				m_onKeyEventListeners[ code ][ mode ] = [];
+				m_onKeyEventListeners[ key ][ mode ] = [];
 			}
 		}
 	}
