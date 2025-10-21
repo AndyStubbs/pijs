b995dbcfd64ea72700daa62fb7ed01396ffa9742
Andy <astubbs50@gmail.com>
2025-10-21 10:29:40 -0600

    Modified setting palette to insert transparent black color as 0 item

--- src/core/colors.js
+++ src/core/colors.js
@@ -6,11 +6,6 @@
  * @module core/colors
  */
 
-// TODO: serDefaultPal and setPal should not set 0'th item to black, it should insert black at
-// position 0 and then add all the colors into the palette. So the indices will shift by 1, but
-// at least the user will not have to add an unused color to the palette when setting palette
-// colors. Also make sure to updat e the defaultPaletteHex in init to remove the 0'th item.
-
 "use strict";
 
 import * as commands from "./commands";
@@ -32,43 +27,38 @@ export function init() {
 
 	// Default 256-color palette (CGA + extended colors) - raw hex strings
 	const defaultPaletteHex = [
-		"#000000", "#0000AA", "#00AA00", "#00AAAA", "#AA0000",
-		"#AA00AA", "#AA5500", "#AAAAAA", "#555555", "#5555FF", "#55FF55", "#55FFFF",
-		"#FF5555", "#FF55FF", "#FFFF55", "#FFFFFF", "#000000", "#141414", "#202020",
-		"#2D2D2D", "#393939", "#454545", "#515151", "#616161", "#717171", "#828282",
-		"#929292", "#A2A2A2", "#B6B6B6", "#CACACA", "#E3E3E3", "#FFFFFF", "#0000FF",
-		"#4100FF", "#7D00FF", "#BE00FF", "#FF00FF", "#FF00BE", "#FF007D", "#FF0041",
-		"#FF0000", "#FF4100", "#FF7D00", "#FFBE00", "#FFFF00", "#BEFF00", "#7DFF00",
-		"#41FF00", "#00FF00", "#00FF41", "#00FF7D", "#00FFBE", "#00FFFF", "#00BEFF",
-		"#007DFF", "#0041FF", "#7D7DFF", "#9E7DFF", "#BE7DFF", "#DF7DFF", "#FF7DFF",
-		"#FF7DDF", "#FF7DBE", "#FF7D9E", "#FF7D7D", "#FF9E7D", "#FFBE7D", "#FFDF7D",
-		"#FFFF7D", "#DFFF7D", "#BEFF7D", "#9EFF7D", "#7DFF7D", "#7DFF9E", "#7DFFBE",
-		"#7DFFDF", "#7DFFFF", "#7DDFFF", "#7DBEFF", "#7D9EFF", "#B6B6FF", "#C6B6FF",
-		"#DBB6FF", "#EBB6FF", "#FFB6FF", "#FFB6EB", "#FFB6DB", "#FFB6C6", "#FFB6B6",
-		"#FFC6B6", "#FFDBB6", "#FFEBB6", "#FFFFB6", "#EBFFB6", "#DBFFB6", "#C6FFB6",
-		"#B6FFB6", "#B6FFC6", "#B6FFDB", "#B6FFEB", "#B6FFFF", "#B6EBFF", "#B6DBFF",
-		"#B6C6FF", "#000071", "#1C0071", "#390071", "#550071", "#710071", "#710055",
-		"#710039", "#71001C", "#710000", "#711C00", "#713900", "#715500", "#717100",
-		"#557100", "#397100", "#1C7100", "#007100", "#00711C", "#007139", "#007155",
-		"#007171", "#005571", "#003971", "#001C71", "#393971", "#453971", "#553971",
-		"#613971", "#713971", "#713961", "#713955", "#713945", "#713939", "#714539",
-		"#715539", "#716139", "#717139", "#617139", "#557139", "#457139", "#397139",
-		"#397145", "#397155", "#397161", "#397171", "#396171", "#395571", "#394571",
-		"#515171", "#595171", "#615171", "#695171", "#715171", "#715169", "#715161",
-		"#715159", "#715151", "#715951", "#716151", "#716951", "#717151", "#697151",
-		"#617151", "#597151", "#517151", "#517159", "#517161", "#517169", "#517171",
-		"#516971", "#516171", "#515971", "#000041", "#100041", "#200041", "#310041",
-		"#410041", "#410031", "#410020", "#410010", "#410000", "#411000", "#412000",
-		"#413100", "#414100", "#314100", "#204100", "#104100", "#004100", "#004110",
-		"#004120", "#004131", "#004141", "#003141", "#002041", "#001041", "#202041",
-		"#282041", "#312041", "#392041", "#412041", "#412039", "#412031", "#412028",
-		"#412020", "#412820", "#413120", "#413920", "#414120", "#394120", "#314120",
-		"#284120", "#204120", "#204128", "#204131", "#204139", "#204141", "#203941",
-		"#203141", "#202841", "#2D2D41", "#312D41", "#352D41", "#3D2D41", "#412D41",
-		"#412D3D", "#412D35", "#412D31", "#412D2D", "#41312D", "#41352D", "#413D2D",
-		"#41412D", "#3D412D", "#35412D", "#31412D", "#2D412D", "#2D4131", "#2D4135",
-		"#2D413D", "#2D4141", "#2D3D41", "#2D3541", "#2D3141", "#000000", "#000000",
-		"#000000", "#000000", "#000000", "#000000", "#000000"
+		"#0000AA", "#00AA00", "#00AAAA", "#AA0000", "#AA00AA", "#AA5500", "#AAAAAA", "#555555",
+		"#5555FF", "#55FF55", "#55FFFF", "#FF5555", "#FF55FF", "#FFFF55", "#FFFFFF", "#000000",
+		"#141414", "#202020", "#2D2D2D", "#393939", "#454545", "#515151", "#616161", "#717171",
+		"#828282", "#929292", "#A2A2A2", "#B6B6B6", "#CACACA", "#E3E3E3", "#FFFFFF", "#0000FF",
+		"#4100FF", "#7D00FF", "#BE00FF", "#FF00FF", "#FF00BE", "#FF007D", "#FF0041", "#FF0000",
+		"#FF4100", "#FF7D00", "#FFBE00", "#FFFF00", "#BEFF00", "#7DFF00", "#41FF00", "#00FF00",
+		"#00FF41", "#00FF7D", "#00FFBE", "#00FFFF", "#00BEFF", "#007DFF", "#0041FF", "#7D7DFF",
+		"#9E7DFF", "#BE7DFF", "#DF7DFF", "#FF7DFF", "#FF7DDF", "#FF7DBE", "#FF7D9E", "#FF7D7D",
+		"#FF9E7D", "#FFBE7D", "#FFDF7D", "#FFFF7D", "#DFFF7D", "#BEFF7D", "#9EFF7D", "#7DFF7D",
+		"#7DFF9E", "#7DFFBE", "#7DFFDF", "#7DFFFF", "#7DDFFF", "#7DBEFF", "#7D9EFF", "#B6B6FF",
+		"#C6B6FF", "#DBB6FF", "#EBB6FF", "#FFB6FF", "#FFB6EB", "#FFB6DB", "#FFB6C6", "#FFB6B6",
+		"#FFC6B6", "#FFDBB6", "#FFEBB6", "#FFFFB6", "#EBFFB6", "#DBFFB6", "#C6FFB6", "#B6FFB6",
+		"#B6FFC6", "#B6FFDB", "#B6FFEB", "#B6FFFF", "#B6EBFF", "#B6DBFF", "#B6C6FF", "#000071",
+		"#1C0071", "#390071", "#550071", "#710071", "#710055", "#710039", "#71001C", "#710000",
+		"#711C00", "#713900", "#715500", "#717100", "#557100", "#397100", "#1C7100", "#007100",
+		"#00711C", "#007139", "#007155", "#007171", "#005571", "#003971", "#001C71", "#393971",
+		"#453971", "#553971", "#613971", "#713971", "#713961", "#713955", "#713945", "#713939",
+		"#714539", "#715539", "#716139", "#717139", "#617139", "#557139", "#457139", "#397139",
+		"#397145", "#397155", "#397161", "#397171", "#396171", "#395571", "#394571", "#515171",
+		"#595171", "#615171", "#695171", "#715171", "#715169", "#715161", "#715159", "#715151",
+		"#715951", "#716151", "#716951", "#717151", "#697151", "#617151", "#597151", "#517151",
+		"#517159", "#517161", "#517169", "#517171", "#516971", "#516171", "#515971", "#000041",
+		"#100041", "#200041", "#310041", "#410041", "#410031", "#410020", "#410010", "#410000",
+		"#411000", "#412000", "#413100", "#414100", "#314100", "#204100", "#104100", "#004100",
+		"#004110", "#004120", "#004131", "#004141", "#003141", "#002041", "#001041", "#202041",
+		"#282041", "#312041", "#392041", "#412041", "#412039", "#412031", "#412028", "#412020",
+		"#412820", "#413120", "#413920", "#414120", "#394120", "#314120", "#284120", "#204120",
+		"#204128", "#204131", "#204139", "#204141", "#203941", "#203141", "#202841", "#2D2D41",
+		"#312D41", "#352D41", "#3D2D41", "#412D41", "#412D3D", "#412D35", "#412D31", "#412D2D",
+		"#41312D", "#41352D", "#413D2D", "#41412D", "#3D412D", "#35412D", "#31412D", "#2D412D",
+		"#2D4131", "#2D4135", "#2D413D", "#2D4141", "#2D3D41", "#2D3541", "#2D3141", "#000000",
+		"#000000", "#000000", "#000000", "#000000", "#000000", "#000000"
 	];
 
 	// Set the default pal and color
@@ -136,39 +126,32 @@ function setDefaultPal( options ) {
 	const pal = options.pal;
 
 	if( !Array.isArray( pal ) ) {
-		const error = new TypeError( "setDefaultPal: parameter pal is not an array." );
+		const error = new TypeError( "setDefaultPal: Parameter pal must be an array." );
 		error.code = "INVALID_PARAMETER";
 		throw error;
 	}
 
-	if( pal.length < 2 ) {
+	if( pal.length === 0 ) {
 		const error = new RangeError(
-			"setDefaultPal: parameter pal must have at least two color values."
+			"setDefaultPal: Parameter pal must have at least one color value."
 		);
 		error.code = "EMPTY_PALETTE";
 		throw error;
 	}
 
-	m_defaultPal = [];
+	// Create default pal with the 0'th item set as a black transparent color
+	m_defaultPal = [ utils.convertToColor( [ 0, 0, 0, 0 ] ) ];
 
+	// Convert palette to color format
 	for( let i = 0; i < pal.length; i++ ) {
 		const c = utils.convertToColor( pal[ i ] );
 		if( c === null ) {
-			console.warn( "setDefaultPal: invalid color value inside array pal." );
+			console.warn( `setDefaultPal: Invalid color value inside array pal at index: ${i}.` );
 			m_defaultPal.push( utils.convertToColor( "#000000" ) );
 		} else {
 			m_defaultPal.push( c );
 		}
 	}
-
-	// Set color 0 to transparent
-	const firstColor = m_defaultPal[ 0 ];
-	m_defaultPal[ 0 ] = utils.convertToColor( [
-		firstColor.r,
-		firstColor.g,
-		firstColor.b,
-		0
-	] );
 
 	// Make sure default color is in the new palette
 	const defaultColorIndex = findColorIndex( m_defaultColor, m_defaultPal, 1 );
@@ -321,7 +304,16 @@ function setPalColor( screenData, options ) {
 		index < 0 ||
 		index >= screenData.pal.length
 	) {
-		const error = new RangeError( "setPalColor: index is not a valid integer value." );
+		const error = new RangeError( "setPalColor: Parameter index is not a valid integer value." );
+		error.code = "INVALID_INDEX";
+		throw error;
+	}
+
+	if( index === 0 ) {
+		const error = new RangeError(
+			"setPalColor: Parameter index cannot be 0, this is reserved for transparency. To set " +
+			"background color of the screen use the setBgColor command."
+		);
 		error.code = "INVALID_INDEX";
 		throw error;
 	}
@@ -329,7 +321,7 @@ function setPalColor( screenData, options ) {
 	const colorValue = utils.convertToColor( color );
 	if( colorValue === null ) {
 		const error = new TypeError(
-			"setPalColor: parameter color is not a valid color format."
+			"setPalColor: Parameter color is not a valid color format."
 		);
 		error.code = "INVALID_COLOR";
 		throw error;
@@ -356,7 +348,23 @@ function setPalColor( screenData, options ) {
 // Get palette
 screenManager.addCommand( "getPal", getPal, [] );
 function getPal( screenData ) {
-	return structuredClone( screenData.pal );
+	const filteredPal = [];
+	for( let i = 1; i < screenData.pal.length; i += 1 ) {
+		const color = screenData.pal[ i ];
+		filteredPal.push( utils.rgbToColor( color.r, color.g, color.b, color.a ) )
+	}
+	return filteredPal;
+}
+
+// Get palette with 0'th item transparent black color
+screenManager.addCommand( "getPalInternal", getPalInternal, [] );
+function getPalInternal( screenData ) {
+	const filteredPal = [];
+	for( let i = 0; i < screenData.pal.length; i += 1 ) {
+		const color = screenData.pal[ i ];
+		filteredPal.push( utils.rgbToColor( color.r, color.g, color.b, color.a ) )
+	}
+	return filteredPal;
 }
 
 // Set entire palette
@@ -365,41 +373,32 @@ function setPal( screenData, options ) {
 	const pal = options.pal;
 
 	if( !Array.isArray( pal ) ) {
-		const error = new TypeError( "setPal: parameter pal is not an array." );
+		const error = new TypeError( "setPal: Parameter pal is must be an array." );
 		error.code = "INVALID_PARAMETER";
 		throw error;
 	}
 
-	if( pal.length < 2 ) {
+	if( pal.length === 0 ) {
 		const error = new RangeError(
-			"setPal: parameter pal must have at least two color values."
+			"setPal: Parameter pal must have at least one color value."
 		);
 		error.code = "EMPTY_PALETTE";
 		throw error;
 	}
 
+	// Create a new pal with 0'th color set to black transparent
+	const newPal = [ utils.convertToColor( [ 0, 0, 0, 0 ] ) ];
+
 	// Convert all colors and validate
-	const newPal = [];
 	for( let i = 0; i < pal.length; i++ ) {
 		const c = utils.convertToColor( pal[ i ] );
 		if( c === null ) {
-			const error = new TypeError(
-				`setPal: invalid color value at index ${i} in array pal.`
-			);
-			error.code = "INVALID_COLOR";
-			throw error;
+			console.warn( `setDefaultPal: Invalid color value inside array pal at index: ${i}.` );
+			newPal.push( utils.convertToColor( "#000000" ) );
+		} else {
+			newPal.push( c );
 		}
-		newPal.push( c );
 	}
-
-	// Set color 0 to transparent (first color should always be transparent)
-	const firstColor = newPal[ 0 ];
-	newPal[ 0 ] = utils.convertToColor( [
-		firstColor.r,
-		firstColor.g,
-		firstColor.b,
-		0
-	] );
 
 	// Set the new palette
 	screenData.pal = newPal;
