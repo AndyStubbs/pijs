140a74dc42102de05316c68fa985792049d905e9
Andy <astubbs50@gmail.com>
2025-10-23 20:11:07 -0600

    Added ops list for line.js

--- test/performance/src/tests/line.js
+++ test/performance/src/tests/line.js
@@ -10,6 +10,7 @@
 
 let m_pal = null;
 let m_seededRandom = null;
+let m_operations = [];
 
 /**
  * Gets the line test configuration object
@@ -36,6 +37,47 @@ function init() {
 	m_seededRandom = new Math.seedrandom( "line", true );
 	
 	m_pal = $.getPal();
+	generateOperationList();
+}
+
+/**
+ * Generates a pre-seeded list of 5000 line operations with parameters
+ * 
+ * @returns {void}
+ */
+function generateOperationList() {
+	m_operations = [];
+	
+	for( let i = 0; i < 5000; i++ ) {
+		const operation = generateRandomOperation();
+		m_operations.push( operation );
+	}
+}
+
+/**
+ * Generates a random line operation with parameters
+ * 
+ * @returns {Object} Operation object with function and parameters
+ */
+function generateRandomOperation() {
+	const width = $.width();
+	const height = $.height();
+	
+	const x1 = Math.floor( m_seededRandom() * width );
+	const y1 = Math.floor( m_seededRandom() * height );
+	const x2 = Math.floor( m_seededRandom() * width );
+	const y2 = Math.floor( m_seededRandom() * height );
+	
+	return {
+		"func": $.line,
+		"params": [ x1, y1, x2, y2 ],
+		"getParams": () => [
+			x1 + Math.floor( Math.random() * 3 ) - 1,
+			y1 + Math.floor( Math.random() * 3 ) - 1,
+			x2 + Math.floor( Math.random() * 3 ) - 1,
+			y2 + Math.floor( Math.random() * 3 ) - 1
+		]
+	};
 }
 
 /**
@@ -47,13 +89,17 @@ function init() {
  */
 function run( itemCount ) {
 	$.cls();
-	for( let i = 0; i < itemCount; i += 1 ) {
+	
+	for( let i = 0; i < itemCount; i++ ) {
+		// Cycle through the pre-generated operations
+		const operationIndex = i % m_operations.length;
+		const operation = m_operations[ operationIndex ];
+		
+		// Set random color
 		$.setColor( Math.floor( Math.random() * m_pal.length ) );
-		$.line(
-			Math.floor( m_seededRandom() * $.width() ) + Math.floor( Math.random() * 3 ) - 1,
-			Math.floor( m_seededRandom() * $.height() ) + Math.floor( Math.random() * 3 ) - 1,
-			Math.floor( m_seededRandom() * $.width() ) + Math.floor( Math.random() * 3 ) - 1,
-			Math.floor( m_seededRandom() * $.height() ) + Math.floor( Math.random() * 3 ) - 1
-		);
+		
+		// Execute the operation with variable parameters to prevent JIT optimization
+		const params = operation.getParams ? operation.getParams() : operation.params;
+		operation.func( ...params );
 	}
 }
