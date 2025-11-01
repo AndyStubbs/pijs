03ebc34b33bd7c6239d75bbc99d0241145d3b23d
Andy <astubbs50@gmail.com>
2025-11-01 17:14:42 -0600

    Added offset adjustments based on even or odd sizes

--- src/graphics/graphics-api.js
+++ src/graphics/graphics-api.js
@@ -76,11 +76,19 @@ export function rebuildApi( s_screenData ) {
 		};
 	} else if( s_penType === g_pens.PEN_SQUARE ) {
 
-		// Square pen
-		const offset = Math.floor( s_penSize / 2 );
+		// Square pen - prefer top/left for even sizes, MCA consistency for odd
+		let s_offsetX;
+		let s_offsetY;
+		if( s_penSize % 2 === 0 ) {
+			s_offsetX = Math.floor( s_penSize / 2 ) - 1;
+			s_offsetY = Math.floor( s_penSize / 2 );
+		} else {
+			s_offsetX = Math.floor( s_penSize / 2 );
+			s_offsetY = Math.floor( s_penSize / 2 ) + 1;
+		}
 		s_psetDrawFn = ( x, y, color ) => {
 			s_drawFilledRectUnsafe(
-				s_screenData, x - offset, y - offset, s_penSize, s_penSize, color
+				s_screenData, x - s_offsetX, y - s_offsetY, s_penSize, s_penSize, color
 			);
 		};
 	} else if( s_penType === g_pens.PEN_CIRCLE ) {
